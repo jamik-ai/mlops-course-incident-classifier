073fc6756da76e8aa0cc72b6c372a3d39ee6f1e6
ievlev-k <84603041+ievlev-k@users.noreply.github.com>
2026-04-28 15:16:00 +0300

    fix: add test

---
+++ tests/test_api.py
@@ -0,0 +1,12 @@
+import pytest
+from fastapi.testclient import TestClient
+from src.api.backend.main import app
+
+client = TestClient(app)
+
+def test_root_endpoint():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {
+        "message": "Call Volume Forecast API. Use POST /api/forecast"
+    }
