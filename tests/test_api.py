369406014a88504cd037aacc13f7ec123aad558e
ievlev-k <84603041+ievlev-k@users.noreply.github.com>
2026-04-28 15:26:13 +0300

    fix: fix test

--- tests/test_api.py
+++ tests/test_api.py
@@ -6,6 +6,4 @@ client = TestClient(app)
 def test_root_endpoint():
     response = client.get("/")
     assert response.status_code == 200
-    assert response.json() == {
-        "message": "Call Volume Forecast API. Use POST /api/forecast"
-    }
+    assert response.json()["message"] == "Call Volume Forecast API. Use POST /api/forecast"
